b0fa376a47269c911ee2f05a397f4259ae1c965c
MarkoBojanic18 <markobojanic18@gmail.com>
2022-01-04 00:40:45 +0100

    Odradjen je Hero deo, deo koji se nalazi ispod navabra

---
+++ src/components/Hero.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { FaDatabase, FaAsterisk, FaAccusoft, FaFacebook, FaTwitter, FaGithub } from 'react-icons/fa'
+import './HeroStyle.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='container'>
+                <div className='content'>
+                    <div className='col-1'>
+                        <h1>Data to enrich your</h1>
+                        <h1><span className='primary-color'>Online business</span></h1>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                        <div className='used-by'>
+                            <p>USED BY</p>
+                            <div className='icons'>
+                                <i><FaDatabase /> Staxx</i>
+                                <i><FaAsterisk /> Star</i>
+                                <i><FaAccusoft /> Accusoft</i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-2'>
+                        <div className='form-layout'>
+                            <div className='form-container'>
+                                    <p className='sign-in-txt'>Sign in with</p>
+                                    <div className='social-login'>
+                                        <i><FaFacebook size={20}/></i>
+                                        <i><FaTwitter size={20}/></i>
+                                        <i><FaGithub size={20}/></i>
+                                    </div>
+                                    <div className='divider'>
+                                        <p><span>Or</span></p>
+                                    </div>
+                                    <form action=''>
+                                        <input type='text' placeholder='Name' />
+                                        <input type='email' placeholder='Email' />
+                                        <input type='password' placeholder='Password' />
+                                        <button>Create your account</button>
+                                    </form>
+                            </div>
+                            <div className='form-footer'>
+                                    <p>By signing up, you agree to our
+                                        <span className='primary-color'> Terms,Data Policy</span> and
+                                        <span className='primary-color'> Cookies Policy</span>.
+                                    </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Hero
