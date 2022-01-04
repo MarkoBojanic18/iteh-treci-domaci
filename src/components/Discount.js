98348f5c26a20f2ac169c75fa2d1c5b0ebb8aa53
MarkoBojanic18 <markobojanic18@gmail.com>
2022-01-04 17:31:40 +0100

    Discount deo odradjen, dodat je popust za praznike

--- src/components/Discount.js
+++ src/components/Discount.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import './DiscountStyle.css'
+import { Link } from 'react-router-dom'
+import tree from '../assest/christmasTree.png'
 
 
 const Discount = () => {
+    var countDate = new Date('Jan 15, 2022 00:00:00').getTime();
+function endOfDiscout() {
+    var now = new Date().getTime();
+    var gap = countDate - now;
+
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+
+    var d = Math.floor(gap / (day));
+    var h = Math.floor((gap % (day)) / (hour));
+    var m = Math.floor((gap % (hour)) / (minute));
+    var s = Math.floor((gap % (minute)) / second);
+
+    document.getElementById("day").innerText = d;
+    document.getElementById("hour").innerText = h;
+    document.getElementById("minute").innerText = m;
+    document.getElementById("second").innerText = s;
+
+}
+
+setInterval(function () {
+    endOfDiscout();
+}, 1000)
     return (
+
         <div class="discount">
+         
         <div class="video">
-            
+        <Link to='/'><img src={tree} alt='' /></Link>
         </div>
         <div class="rightSide">
             <div class="tajmer">
